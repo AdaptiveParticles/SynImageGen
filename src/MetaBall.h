ba4fb145ee72199a47964fac565d931d85af9ba0
Bevan Cheeseman <cheesema@mpi-cbg.de>
2017-01-26 19:06:50 +0100

    bug in threshold

--- src/MetaBall.h
+++ src/MetaBall.h
@@ -12,6 +12,7 @@
 
 class MetaBall {
 
+public:
 
     float x0;
     float y0;
@@ -22,32 +23,36 @@ class MetaBall {
     MetaBall(float x0,float y0,float z0,float mag): y0(y0), x0(x0), z0(z0),mag(mag) {
     }
 
-};
+    float calculate_potential(float x,float y,float z){
+        //
+        // Reproduced from http://www.geisswerks.com/ryan/BLOBS/blobs.html
+        //
+        //
 
-float MetaBall::calculate_potential(float x,float y,float z){
-    //
-    // Reproduced from http://www.geisswerks.com/ryan/BLOBS/blobs.html
-    //
-    //
+        float dx = (x - x0);
+        float dy = (y - y0);
+        float dz = (z - z0);
+        float r_squared = (dx*dx + dy*dy + dz*dz);
 
-    float dx = (x - x0);
-    float dy = (y - y0);
-    float dz = (z - z0);
-    float r_squared = (dx*dx + dy*dy + dz*dz);
+        float q = 0;
 
-    //enable this line if your blobs are of varying sizes:
-    r_squared /= (mag*mag);
+        //enable this line if your blobs are of varying sizes:
+        r_squared /= (mag*mag);
 
-    // since f(r) is valid when r is in the range [0-.707],
-    // r_squared should be in the range [0-.500].
-    if (r_squared < 0.5f)     // same as: if (sqrtf(r_squared) < 0.707f)
-    {
-        q += (0.25 - r_squared + r_squared*r_squared);
+        // since f(r) is valid when r is in the range [0-.707],
+        // r_squared should be in the range [0-.500].
+        if (r_squared < 0.5f)     // same as: if (sqrtf(r_squared) < 0.707f)
+        {
+            q += (0.25 - r_squared + r_squared*r_squared);
+        }
+
+        return q;
+
     }
 
-    return q;
+};
 
-}
+
 
 void generate_metaball_template(Object_template& obj_template,int sample_rate,float real_size,float density = 1000000,float rad_ratio = 3.0/8.0){
     //
@@ -94,9 +99,9 @@ void generate_metaball_template(Object_template& obj_template,int sample_rate,fl
 
         float mag = grand.rand_num(.2*radius_sphere,.5*radius_sphere);
 
-        float x0 = grand.rand_num(mag,basic_sphere.true_object_distribution.x_num-mag);
-        float y0 = grand.rand_num(mag,basic_sphere.true_object_distribution.y_num-mag);
-        float z0 = grand.rand_num(mag,basic_sphere.true_object_distribution.z_num-mag);
+        float x0 = grand.rand_num(mag,obj_template.true_object_distribution.x_num-mag);
+        float y0 = grand.rand_num(mag,obj_template.true_object_distribution.y_num-mag);
+        float z0 = grand.rand_num(mag,obj_template.true_object_distribution.z_num-mag);
 
         MetaBall temp_ball(x0,y0,z0,mag);
 
@@ -106,12 +111,12 @@ void generate_metaball_template(Object_template& obj_template,int sample_rate,fl
 
     // loop over and create field
 
-    for (int i = 0; i < basic_sphere.true_object_distribution.y_num; i++) {
-        for (int j = 0; j < basic_sphere.true_object_distribution.x_num; j++) {
-            for (int k = 0; k < basic_sphere.true_object_distribution.z_num; k++) {
+    for (int i = 0; i < obj_template.true_object_distribution.y_num; i++) {
+        for (int j = 0; j < obj_template.true_object_distribution.x_num; j++) {
+            for (int k = 0; k < obj_template.true_object_distribution.z_num; k++) {
 
                 for (int l = 0; l < num_meta_balls; ++l) {
-                    potential_field(i,j,k) += balls[l].calculate_potential(i,j,k);
+                    potential_field(i,j,k) += balls[l].calculate_potential((float)i,(float)j,(float)k);
                 }
 
             }
@@ -122,14 +127,14 @@ void generate_metaball_template(Object_template& obj_template,int sample_rate,fl
     //need threshold now the generated field
     float threshold = 0.15;
 
-    for (int i = 0; i < basic_sphere.true_object_distribution.y_num; i++) {
-        for (int j = 0; j < basic_sphere.true_object_distribution.x_num; j++) {
-            for (int k = 0; k < basic_sphere.true_object_distribution.z_num; k++) {
+    for (int i = 0; i < obj_template.true_object_distribution.y_num; i++) {
+        for (int j = 0; j < obj_template.true_object_distribution.x_num; j++) {
+            for (int k = 0; k < obj_template.true_object_distribution.z_num; k++) {
 
-                if (potential_field < threshold) {
-                    basic_sphere.true_object_distribution(i,j,k) = 1*basic_sphere.max_sample;
+                if (potential_field(i,j,k) > threshold) {
+                    obj_template.true_object_distribution(i,j,k) = 1*obj_template.max_sample;
                 } else {
-                    basic_sphere.true_object_distribution(i,j,k) = 0;
+                    obj_template.true_object_distribution(i,j,k) = 0;
                 }
             }
         }
